0ca1e41237a82a83f4862f30a4a6f2555c28e316
takuaraki <takuaraki7@gmail.com>
2019-07-06 02:48:47 +0900

    add module: KotlinCoroutines

---
+++ kotlin_coroutines/src/main/java/com/example/kotlin_coroutines/KotlinCoroutinesActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kotlin_coroutines
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KotlinCoroutinesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kotlin_coroutines)
+    }
+}
