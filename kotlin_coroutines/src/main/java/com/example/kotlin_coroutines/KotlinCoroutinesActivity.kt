e8ece14d1ed88842c1554928d0238fa295e64075
takuaraki <takuaraki7@gmail.com>
2019-08-12 00:14:44 +0900

    my first coroutines

--- kotlin_coroutines/src/main/java/com/example/kotlin_coroutines/KotlinCoroutinesActivity.kt
+++ kotlin_coroutines/src/main/java/com/example/kotlin_coroutines/KotlinCoroutinesActivity.kt
@@ -2,11 +2,23 @@ package com.example.kotlin_coroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_kotlin_coroutines.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class KotlinCoroutinesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin_coroutines)
+
+        fetchButton.setOnClickListener {
+            GlobalScope.launch { // launch a new coroutine in background and continue
+                delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
+                println("World!") // print after delay
+            }
+            println("Hello,") // main thread continues while coroutine is delayed
+        }
     }
 }
