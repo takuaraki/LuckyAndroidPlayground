b3880800e505cc56607f9eec2eb7afea67ea088d
takuaraki <takuaraki7@gmail.com>
2019-09-08 16:39:41 +0900

    add module: CameraX_OpenGL

---
+++ camerax_opengl/src/main/java/com/example/camerax_opengl/CameraxOpenGLActivity.kt
@@ -0,0 +1,12 @@
+package com.example.camerax_opengl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CameraxOpenGLActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camerax_open_gl)
+    }
+}
